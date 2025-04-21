a01b785e81ddda66c39455cdce8dd1030ce92a4d
anggasulistiangga <anggasulistiangga@gmail.com>
2025-04-22 00:51:48 +0700

    first commit

---
+++ Models/KaryawanFormViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplikasiKaryawanApp.ViewModels
+{
+    public class KaryawanFormViewModel
+    {
+        public Karyawan Karyawan { get; set; }
+        public List<Perusahaan> Perusahaan { get; set; }
+        public List<Cabang> Cabang { get; set; }
+    }
+}
